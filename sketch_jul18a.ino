0018c1d189dbdca307f3dc79c3948bed2a0c5b38
Arijeet Kar <kararijeet26@gmail.com>
2023-07-26 00:00:18 +0530

    Update sketch_jul18a.ino

--- sketch_jul18a.ino
+++ sketch_jul18a.ino
@@ -1,6 +1,26 @@
+int button1=11;
+int button2=12;
+int led=13;
+int buttonState1=0;
+int buttonState2=0;
 void setup(){
-
+  pinMode(button1,INPUT);
+  pinMode(button2,INPUT);
+  pinMode(led,OUTPUT);
 }
 void loop(){
-  
+  buttonState1= digitalRead(button1);  
+  buttonState2= digitalRead(button2);
+  if(buttonState1 == LOW || buttonState2 == LOW)
+  {
+    delay(1000);
+    digitalWrite(led,HIGH);
+    delay(1000);
+  }
+  else
+  {
+    delay(1000);
+    digitalWrite(led,LOW);
+    delay(1000);
+  }
 }
